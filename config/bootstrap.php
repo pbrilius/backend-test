c5024c0a4fb675025fe562820aa97dd07a7bf9d4
Povilas Brilius <pbrilius@gmamil.com>
2018-03-07 21:32:37 +0000

    PHPUnit tests running

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,20 +1,7 @@
 <?php
 
-use Composer\Autoload\ClassLoader;
-
-/* @var $loader ClassLoader */
-$loader = require __DIR__ . '/../vendor/autoload.php';
-$loader->add('', '/tests');
-$loader->addClassMap(['tests']);
-$loader->addPsr4('', getcwd() . '/tests');
-$classLoader = new \Composer\Autoload\ClassLoader();
-
-$loader->register();
-
-var_dump($loader->getClassMap());
-var_dump($loader->getPrefixes());
-
-require  __DIR__ . '/metaConfig.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/metaConfig.php';
 
 use DI\ContainerBuilder;
 use Doctrine\Common\Cache\ArrayCache;
