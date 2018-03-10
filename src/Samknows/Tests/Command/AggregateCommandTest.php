c4e6db83e5b51d17bd02b7dfec04793e4259c1e7
Povilas Brilius <pbrilius@gmamil.com>
2018-03-10 16:52:47 +0000

    Aggregate command units passed

--- src/Samknows/Tests/Command/AggregateCommandTest.php
+++ src/Samknows/Tests/Command/AggregateCommandTest.php
@@ -17,7 +17,7 @@ final class AggregateCommandTest extends CommadTestCase
     public function testCanBeCreated(): void
     {
         $container = $this->getContainer();
-        $this->assertEquals(AggregateCommand::class, $container->get(AggregateCommand::class));
+        $this->assertEquals(AggregateCommand::class, get_class($container->get(AggregateCommand::class)));
     }
 
     public function testCanBeNamed()
@@ -25,30 +25,30 @@ final class AggregateCommandTest extends CommadTestCase
         $container = $this->getContainer();
         /* @var $aggregateCommand AggregateCommand */
         $aggregateCommand = $container->get(AggregateCommand::class);
-        $this->assertEquals('app:load-data', $aggregateCommand->getName());
+        $this->assertEquals('app:aggregate', $aggregateCommand->getName());
     }
 
     public function testCanBeCreatedWithArguments()
     {
         $container = $this->getContainer();
         /* @var $aggregateCommand aggregateCommand */
-        $aggregateCommand = $container->get(aggregateCommand::class);
-        $this->assertEqual(0, $aggregateCommand->getDefinition()->getArgumentCount());
+        $aggregateCommand = $container->get(AggregateCommand::class);
+        $this->assertEquals(0, count($aggregateCommand->getDefinition()->getOptions()));
     }
 
     public function testCanBeCreatedWithDescription()
     {
         $container = $this->getContainer();
         /* @var $aggregateCommand aggregateCommand */
-        $aggregateCommand = $container-get(aggregateCommand::class);
-        $this->assertStringMatchesFormat('[\w\s\.]{8,}', $aggregateCommand->getDescription());
+        $aggregateCommand = $container->get(AggregateCommand::class);
+        $this->assertRegExp('/[\w\s\.]{8,}/', $aggregateCommand->getDescription());
     }
 
     public function testExecute()
     {
         $container = $this->getContainer();
         /* @var $aggregateCommand aggregateCommand */
-        $aggregateCommand = $container->get(aggregateCommand::class);
+        $aggregateCommand = $container->get(AggregateCommand::class);
         $application = $this->getApplication();
         $command = $application->find($aggregateCommand->getName());
 
